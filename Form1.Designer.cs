bd2d774a6491587e4b610661dcb63a72e6fe6748
Lenovo <Lenovo@DESKTOP-81DQNOK>
2024-05-18 12:48:45 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace zad2lab2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtDzielna;
+        private System.Windows.Forms.TextBox txtDzielnik;
+        private System.Windows.Forms.TextBox txtWynik;
+        private System.Windows.Forms.Button btnDivide;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtDzielna = new System.Windows.Forms.TextBox();
+            this.txtDzielnik = new System.Windows.Forms.TextBox();
+            this.txtWynik = new System.Windows.Forms.TextBox();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDzielna
+            // 
+            this.txtDzielna.Location = new System.Drawing.Point(12, 12);
+            this.txtDzielna.Name = "txtDzielna";
+            this.txtDzielna.Size = new System.Drawing.Size(100, 20);
+            this.txtDzielna.TabIndex = 0;
+            // 
+            // txtDzielnik
+            // 
+            this.txtDzielnik.Location = new System.Drawing.Point(12, 38);
+            this.txtDzielnik.Name = "txtDzielnik";
+            this.txtDzielnik.Size = new System.Drawing.Size(100, 20);
+            this.txtDzielnik.TabIndex = 1;
+            // 
+            // txtWynik
+            // 
+            this.txtWynik.Location = new System.Drawing.Point(12, 64);
+            this.txtWynik.Name = "txtWynik";
+            this.txtWynik.ReadOnly = true;
+            this.txtWynik.Size = new System.Drawing.Size(100, 20);
+            this.txtWynik.TabIndex = 2;
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(12, 90);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(100, 23);
+            this.btnDivide.TabIndex = 3;
+            this.btnDivide.Text = "Podziel";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.txtWynik);
+            this.Controls.Add(this.txtDzielnik);
+            this.Controls.Add(this.txtDzielna);
+            this.Name = "Form1";
+            this.Text = "Aplikacja Dzielenia";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace zad2lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string dzielnaText = txtDzielna.Text;
+                string dzielnikText = txtDzielnik.Text;
+
+                if (double.TryParse(dzielnaText, out double dzielna) && double.TryParse(dzielnikText, out double dzielnik))
+                {
+                    if (dzielnik == 0)
+                    {
+                        throw new DivideByZeroException("Dzielnik nie może być zerem.");
+                    }
+
+                    double wynik = dzielna / dzielnik;
+
+                    txtWynik.Text = wynik.ToString();
+                }
+                else
+                {
+                    throw new FormatException("Wprowadzone wartości muszą być liczbami.");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogErrorToEventLog(ex);
+
+                MessageBox.Show(ex.Message, "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LogErrorToEventLog(Exception ex)
+        {
+            string source = "DivisionApp";
+            string log = "Application";
+
+            if (!EventLog.SourceExists(source))
+            {
+                EventLog.CreateEventSource(source, log);
+            }
+
+            EventLog.WriteEntry(source, ex.ToString(), EventLogEntryType.Error);
+        }
+    }
+}
